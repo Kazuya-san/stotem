4e97d521ec93cb270258afc619c2723380960369
Kazuya-san <kazuyasan3286@gmail.com>
2022-12-17 21:05:41 +0500

    Added Pagination v2

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,16 +1,11 @@
 import mongoose from "mongoose";
 
-// "mongodb://localhost:27017/EventApp"
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(
-      "mongodb://localhost:27017/EventApp",
-
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    );
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
